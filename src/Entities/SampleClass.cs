cbf77e325e0c2c308e21ab5f5cf9eac0c88747a2
Patrick Kurmann <kurmannwillisau@gmail.com>
2024-04-09 07:28:47 +0200

    created github repo from own template

---
+++ src/Entities/SampleClass.cs
@@ -0,0 +1,10 @@
+﻿namespace Kurmann.Media.Metadata;
+
+public class SampleClass
+{
+    public required string Id { get; set; }
+
+    public required string LastName { get; set; }
+
+    public string? FirstName { get; set; }
+}
